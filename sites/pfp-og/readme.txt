dd51d7bb550461e57732a3ee1616db684bd8ff33
Jesse Lafica <jlafica8@gmail.com>
2019-03-25 21:29:33 -0700

    added copy

---
+++ sites/pfp-og/readme.txt
@@ -0,0 +1,57 @@
+PACKING FOR PEANUTS
+1. Visit and add bootstrap for layout ideas
+2. Create form inputs with device names
+3. Create each of the  scenarios in scripts.js
+4. Create conversion logic for SSRs to POLDs
+5. ADD TERNARY for > max of each to return the need for a POLD box.
+ACCESSORIES
+API
+POLD
+SSR
+4hr Timer / Recirc Pump Switch
+
+*add ability to save failed packaging request as an object for later
+Important Notes:
+2 SSRs = 1 POLD
+
+PHASE 1
+  Create initial application that will only look at the input values and see if they exceed the 13 possible scenarios
+PHASE 2
+  Add support for additional packaging i.e. POLD boxes
+PHASE 3
+  Add support for the rest of the potential packaging i.e. electrical junction boxes, LDS outdoor bags, etc.
+PHASE 4
+  Setup database (using PHP & mySQL?) that will accept comments or requests for changes. App will store failed attempts to package. --->if error is detected email the object using https://formspree.io/
+
+
+13 scenarios"Will they all fit?
+(LDS Small Shipper)"
+            API	    POLD	  SSR's	  Recirc4hr
+Scen1	      0	      18      0      	1
+---------------------------------------------
+Scen2	      1	      18      0      	0
+---------------------------------------------
+Scen3	      1	      1	      2	      3
+---------------------------------------------
+Scen4	      1	      13      0      	1
+---------------------------------------------
+Scen5	      1	      12      2      	1
+---------------------------------------------
+Scen6	      2	      13      0      	0
+---------------------------------------------
+Scen7	      2	      0	      0	      3
+---------------------------------------------
+Scen8	      3	      0	      0	      2
+---------------------------------------------
+Scen9	      3	      7	      0	      1
+---------------------------------------------
+Scen10      3      	10	    0	      0
+---------------------------------------------
+Scen11      4      	0      	0      	2
+---------------------------------------------
+Scen12      6      	0      	0      	1
+---------------------------------------------
+Scen13      7      	0      	0      	0
+
+
+Use the <kbd> to indicate input that is typically entered via keyboard.
---
+++ sites/pfp-og/scripts.js
@@ -0,0 +1,115 @@
+// Initialize variables
+let oneBox = false;
+
+// Inputs
+const apis    = document.querySelector("#api");
+const polds   = document.querySelector("#pold");
+const ssrs    = document.querySelector("#ssr");
+const recircs = document.querySelector("#recirc");
+
+//Buttons
+// const submitBtn = document.querySelector("#submit_button");
+//       submitBtn.addEventListener('click', calcPack, false);
+const resetBtn  = document.querySelector("#reset_button");
+const form      = document.querySelector("#pack_form");
+      form.addEventListener('submit', calcPack, false);
+
+// Message
+const message = document.querySelector("#message");
+
+// Declare functions
+function calcPack(e) {
+  // reinit vars and vals
+  e.preventDefault();
+
+  oneBox = false;
+
+  console.log(apis.value);
+  console.log(polds.value);
+  console.log(ssrs.value);
+  console.log(recircs.value);
+
+  // convert SSRs and add them to POLDs
+
+  // add ternary for conditional >max of each type to end function and return second shipper
+}
+  // before continuing with conditionals for 13 single shipper scenarios
+
+  const oScenario1 = {
+    api    : 0,
+    pold   : 18,
+    ssr    : 0,
+    recirc : 1
+  };
+  const oScenario2 = {
+    api    : 1,
+    pold   : 18,
+    ssr    : 0,
+    recirc : 0
+  };
+  const oScenario3 = {
+    api    : 1,
+    pold   : 1,
+    ssr    : 2,
+    recirc : 3
+  };
+  const oScenario4 = {
+    api    : 1,
+    pold   : 13,
+    ssr    : 0,
+    recirc : 1
+  };
+  const oScenario5 = {
+    api    : 1,
+    pold   : 12,
+    ssr    : 2,
+    recirc : 1
+  };
+  const oScenario6 = {
+    api    : 2,
+    pold   : 13,
+    ssr    : 0,
+    recirc : 0
+  };
+  const oScenario7 = {
+    api    : 2,
+    pold   : 0,
+    ssr    : 0,
+    recirc : 3
+  };
+  const oScenario8 = {
+    api    : 3,
+    pold   : 0,
+    ssr    : 0,
+    recirc : 2
+  };
+  const oScenario9 = {
+    api    : 3,
+    pold   : 7,
+    ssr    : 0,
+    recirc : 1
+  };
+  const oScenario10 = {
+    api    : 3,
+    pold   : 10,
+    ssr    : 0,
+    recirc : 0
+  };
+  const oScenario11 = {
+    api    : 4,
+    pold   : 0,
+    ssr    : 0,
+    recirc : 2
+  };
+  const oScenario12 = {
+    api    : 6,
+    pold   : 0,
+    ssr    : 0,
+    recirc : 1
+  };
+  const oScenario13 = {
+    api    : 7,
+    pold   : 0,
+    ssr    : 0,
+    recirc : 0
+  };
