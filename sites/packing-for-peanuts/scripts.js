06f55f61596db8655e087556d69afcb9ce02c946
jesselafica <35412938+jesselafica@users.noreply.github.com>
2019-03-26 15:31:09 -0700

    scripts, atom-styles, and do_next
    completed basic logic for scripts.js
    added txt file for "to do"
    altered atom styles

--- sites/packing-for-peanuts/scripts.js
+++ sites/packing-for-peanuts/scripts.js
@@ -24,83 +24,101 @@ const scenarios = [
     api    : 0,
     pold   : 18,
     ssr    : 0,
+    poldSsr: 18,
     recirc : 1
   },
   {
     api    : 1,
     pold   : 18,
     ssr    : 0,
+    poldSsr: 18,
     recirc : 0
   },
   {
     api    : 1,
     pold   : 1,
     ssr    : 2,
+    poldSsr: 3,
     recirc : 3
   },
   {
     api    : 1,
     pold   : 13,
     ssr    : 0,
+    poldSsr: 13,
     recirc : 1
   },
   {
     api    : 1,
     pold   : 12,
     ssr    : 2,
+    poldSsr: 14,
     recirc : 1
   },
   {
     api    : 2,
     pold   : 13,
     ssr    : 0,
+    poldSsr: 13,
     recirc : 0
   },
   {
     api    : 2,
     pold   : 0,
     ssr    : 0,
+    poldSsr: 0,
     recirc : 3
   },
   {
     api    : 3,
     pold   : 0,
     ssr    : 0,
+    poldSsr: 0,
     recirc : 2
   },
   {
     api    : 3,
     pold   : 7,
     ssr    : 0,
+    poldSsr: 7,
     recirc : 1
   },
   {
     api    : 3,
     pold   : 10,
     ssr    : 0,
+    poldSsr: 10,
     recirc : 0
   },
   {
     api    : 4,
     pold   : 0,
     ssr    : 0,
+    poldSsr: 0,
     recirc : 2
   },
   {
     api    : 6,
     pold   : 0,
     ssr    : 0,
+    poldSsr: 0,
     recirc : 1
   },
   {
     api    : 7,
     pold   : 0,
     ssr    : 0,
+    poldSsr: 0,
     recirc : 0
   }
 ];
 
 
+function checkpoldSsr(){
+  scenarios.forEach((scenario, ind) => {
+    return((scenario.pold + scenario.ssr) === scenario.poldSsr) ? console.log(ind + " correct") : console.log(ind + "ERROR " + scenario.value);
+  });
+};
 // Declare functions
 function calcPack(e) {
   // reinit vars and vals
@@ -113,8 +131,15 @@ function calcPack(e) {
   formValsObj.ssr = parseInt(ssrs.value);
   formValsObj.recirc = parseInt(recircs.value);
 
+  // add polds and ssrs together
+  formValsObj.poldSsr = formValsObj.pold + (formValsObj.ssr / 2);
+  console.log(formValsObj.poldSsr);
+
   scenarios.forEach((scenario, ind) => {
-    if (JSON.stringify(formValsObj) === JSON.stringify(scenario)) {
+    if (formValsObj.api <= scenario.api
+      && formValsObj.pold <= scenario.pold
+      && formValsObj.ssr <= scenario.ssr
+      && formValsObj.recirc <= scenario.recirc ) {
       console.log("Matched!", ind);
     }
   });
