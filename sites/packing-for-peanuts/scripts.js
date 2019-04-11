9cd2ae916f9f64fd671bc5ec0241bd8dd68c9407
Jesse Lafica <jlafica8@gmail.com>
2019-04-11 12:55:48 -0700

    copy pasted test-branch file contents into their respective files i.e. scripts_branch.js to scripts.js

--- sites/packing-for-peanuts/scripts.js
+++ sites/packing-for-peanuts/scripts.js
@@ -1,10 +1,10 @@
 // Initialize variables
 // Inputs
-const apis    = document.getElementById("api");
-const polds   = document.getElementById("pold");
-const ssrs    = document.getElementById("ssr");
-const recircs = document.getElementById("recirc");
-const ldsSize = document.getElementById("lds_size");
+const apiInput    = document.getElementById("api");
+const poldInput   = document.getElementById("pold");
+const ssrInput    = document.getElementById("ssr");
+const recircInput = document.getElementById("recirc");
+const ldsSize     = document.getElementById("lds_size");
 //Buttons
 // const submitBtn = document.getElementById("submit_button");
 //       submitBtn.addEventListener('click', calcPack, false);
@@ -16,7 +16,7 @@ form.addEventListener('submit', calcPack, false);
 const message = document.getElementById("message");
 // Scenario array objects
 const ldsScenarios  = [
-// START SM SHIPPER
+  // START SM SHIPPER
   {
     api     : 0,
     pold    : 18,
@@ -121,33 +121,33 @@ const ldsScenarios  = [
     recirc : 0,
     ldsSize: 150
   },
-// END SM SHIPPER
-// START BROWN BOX
-{
-  api     : 2,
-  pold    : 0,
-  ssr     : 0,
-  poldSsr : 0,
-  recirc  : 2,
-  ldsSize : 200
-},
-{
-  api     : 1,
-  pold    : 2,
-  ssr     : 0,
-  poldSsr : 2,
-  recirc  : 2,
-  ldsSize : 200
-},
-{
-  api     : 0,
-  pold    : 4,
-  ssr     : 0,
-  poldSsr : 4,
-  recirc  : 2,
-  ldsSize : 200
-}
-// END BROWN BOX
+  // END SM SHIPPER
+  // START BROWN BOX
+  {
+    api     : 2,
+    pold    : 0,
+    ssr     : 0,
+    poldSsr : 0,
+    recirc  : 2,
+    ldsSize : 200
+  },
+  {
+    api     : 1,
+    pold    : 2,
+    ssr     : 0,
+    poldSsr : 2,
+    recirc  : 2,
+    ldsSize : 200
+  },
+  {
+    api     : 0,
+    pold    : 4,
+    ssr     : 0,
+    poldSsr : 4,
+    recirc  : 2,
+    ldsSize : 200
+  }
+  // END BROWN BOX
 ];
 const poldScenarios = [
   {
@@ -253,55 +253,44 @@ function calcPack(e) {
   let shipperSize = "";
   let oneBox      = false;
 
-// create formValsObj from inputs
-  formValsObj.api     = parseInt(apis.value);
-  formValsObj.pold    = parseInt(polds.value);
-  formValsObj.ssr     = parseInt(ssrs.value);
-  formValsObj.recirc  = parseInt(recircs.value);
+  // create formValsObj from inputs
+  formValsObj.api     = parseInt(apiInput.value);
+  formValsObj.pold    = parseInt(poldInput.value);
+  formValsObj.ssr     = parseInt(ssrInput.value);
+  formValsObj.recirc  = parseInt(recircInput.value);
   formValsObj.ldsSize = parseInt(ldsSize.value);
-  formValsObj.poldSsr = formValsObj.pold + (formValsObj.ssr / 2);
+  formValsObj.poldSsr = parseInt(formValsObj.pold + (formValsObj.ssr / 2));
 
-// loop through ldsScenarios test if oneBox = true
-  ldsScenarios.forEach((scenario, ind) => {
-    if (formValsObj.api        <= scenario.api
-     && formValsObj.poldSsr    <= scenario.poldSsr
-     && formValsObj.recirc     <= scenario.recirc
-     && formValsObj.ldsSize    <= scenario.ldsSize)
-    {
-    console.log("Matched!", ind);
+  // loop through ldsScenarios test if oneBox = true
+
+  oneBox = oneBoxFunc();
+  if (oneBox) {
     shipperSize = (formValsObj.ldsSize < 200) ? "19 x 12 x 7" : "14 x 14 x 14";
     message.innerHTML = "One box required:<br>" + shipperSize;
-    return oneBox = true;
   } else if (!oneBox) {
+    message.innerHTML = "Please call the Warehouse for assistance."
 
+  }
 
-      message.innerHTML = "Please call the Warehouse for assistance."
-    }
-  });
-    ldsScenarios.forEach((scenario, ind) =>{
-      let apiMod     = formValsObj.api % scenario.api;
-      let poldSsrMod = formValsObj.poldSsr % scenario.poldSsr;
-      let recircMod  = formValsObj.recirc % scenario.recirc;
+  // Create oneBoxFunc to use for loop through formValsObj <= ldsScenarios
+  function oneBoxFunc() {
+    for (var i = 0; i < ldsScenarios.length; i++) {
+      if (formValsObj.api        <= ldsScenarios[i].api
+        && formValsObj.poldSsr    <= ldsScenarios[i].poldSsr
+        && formValsObj.recirc     <= ldsScenarios[i].recirc
+        && formValsObj.ldsSize    <= ldsScenarios[i].ldsSize)
+        {
+          return oneBox = true;
+          break;
+        }
+      }
+    } // End oneBoxFunc
 
-      console.log(apiMod);
-      console.log(poldSsrMod);
-      console.log(recircMod);
-      // create scenarioRemainder{} {remainder : 0, index: =ind}
-      // loop through scenario[]
-      // += each remainder to a temporary variable (tempRemainder) (i.e. formValsObj.api - scenario.api, etc.)
-      // if tempRemainder is < scenarioRemainder.tempRemainder
-      // -->then scenarioRemainder.remainder = tempRemainder & add scenario index to scenarioRemainder.scenario
+} // End calcPack
+
+  // tool check scenario object poldSsr count
+  function checkPoldSsr(scenArray){
+    scenArray.forEach((scenario, ind) => {
+      return((scenario.pold + (scenario.ssr / 2)) === scenario.poldSsr) ? console.log(ind + " correct") : console.log("Check scenario at index " + ind);
     });
-    // find scenario with smallest % (modulo) && >= all scenario.keys (not <)
-    // subtract scenario.keys from formValsObj.keys
-    // divide remaining formValsObj.keys values into poldScenario.keys until all formValsObj.keys <= poldScenario.keys
-    // round up number of times remaining formValsObj divided into poldScenario to determine # of boxes required to ship
-
-}
-
-// tool check scenario object poldSsr count
-function checkPoldSsr(scenArray){
-  scenArray.forEach((scenario, ind) => {
-    return((scenario.pold + (scenario.ssr / 2)) === scenario.poldSsr) ? console.log(ind + " correct") : console.log("Check scenario at index " + ind);
-  });
-};
+  };
