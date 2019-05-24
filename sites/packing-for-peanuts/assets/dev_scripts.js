0f232ef90f3a4b716d9dcf46a5090fff8e0e2311
Jesse Lafica <jlafica8@gmail.com>
2019-05-23 20:54:52 -0700

    shipperCalc tool added, new ldsTypes arr

---
+++ sites/packing-for-peanuts/assets/dev_scripts.js
@@ -0,0 +1,37 @@
+const ldsTypes = document.getElementsByClassName('lds-type');
+
+function calcPack(e) {
+// prevent default submit function
+  e.preventDefault();
+  // ===================================================
+  // I N I T I A L I Z E - V A R I A B L E S
+  // ===================================================
+  const formValsObj = {};
+  let oneBox        = false;
+  let twoBox        = false;
+  // create formValsObj from inputs
+  formValsObj.api     = parseInt(apiInput.value);
+  formValsObj.pold    = parseInt(poldInput.value);
+  formValsObj.ssr     = parseInt(ssrInput.value);
+  formValsObj.recirc  = parseInt(recircInput.value);
+  formValsObj.ldsSize = parseInt(ldsSize.value);
+  formValsObj.poldSsr = parseInt(formValsObj.pold + (formValsObj.ssr / 2));
+  // determine shipper size based on lds type radio button
+  let shipperSize   = '';
+  const shipperCalc = () => {
+    for (let i = 0; i < ldsTypes.length; i++) {
+      let radioClasses = ldsTypes[i].classList;
+      for (let c = 0; c < radioClasses.length; c++) {
+        if (radioClasses[c] === 'active') {
+          shipperSize = (ldsTypes[i].getAttribute('id') === 'lds_radio') ? '19 x 12 x 7' : '13 x 10 x 5';
+          console.log(shipperSize);
+          break;
+        }
+      }
+    }
+  }
+  // ===================================================
+  //
+  // ===================================================
+
+}
